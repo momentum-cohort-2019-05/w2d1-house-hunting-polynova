b518e28fee256f9bf23d1755245fcabf1cfd9653
polynova <jocelyncasanova1@gmail.com>
2019-06-04 09:42:27 -0400

    added new house hw

---
+++ househunting.py
@@ -0,0 +1,26 @@
+
+
+
+annual_salary = input ("What is your annual salary? ")
+print ("Your annual salary is ", annual_salary)
+   
+total_cost = input ("What does your total dream house cost?")
+print ("Your dream house cost is ", total_cost)
+
+portion_down_payment = input ("What is your down payment? ")
+print ("Your down payment is ", portion_down_payment)
+
+current_savings = input( "What is your current savings?")
+pring ("Your current savings is", current_savings)
+
+
+
+
+
+portion_down_payment = total_cost * float( input( " what is the percentage on your annual return rate?"))
+r = float(input(what is your expected rate of return ))
+
+num_months = 0 
+
+while current_savings < portion_down_payment:
+    current_savings = (current_savings + (current_savings * (r/12) + (annual_salary / 12 ) * portion_saved))
